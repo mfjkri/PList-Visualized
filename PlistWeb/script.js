ac20d5631e70d124a5db11756c9dad525e9d7684
mfjkri <muhammadfikri6844@gmail.com>
2022-05-17 16:51:22 +0800

    Changed delimeter of output.csv

--- PlistWeb/script.js
+++ PlistWeb/script.js
@@ -186,7 +186,7 @@ function generateForm() {
   });
 
   submitBtn.addEventListener("click", () => {
-    results = "Participant Name, Webex Username\n";
+    results = "Participant Name|Webex Username\n";
 
     if (checkOptionFilled(submitBtn, stateNumber)) {
       selectedOptions = {};
@@ -202,7 +202,7 @@ function generateForm() {
         selectedName = Questions[questionNumber][1][selectedOption];
 
         if (!(selectedName === "  ")) {
-          results += `${participantName} | ${selectedName}\n`;
+          results += `${participantName}|${selectedName}\n`;
         }
       });
 
